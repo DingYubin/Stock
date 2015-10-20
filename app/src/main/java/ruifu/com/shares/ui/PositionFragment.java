87635c4fda8aef7cf21bea213aa5e0c76a4811a4
yubin.ding <yubin.ding@ruifusoft.com>
2015-10-20 20:16:48 +0800

    add transaction ui

---
+++ app/src/main/java/ruifu/com/shares/ui/PositionFragment.java
@@ -0,0 +1,32 @@
+package ruifu.com.shares.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ruifu.com.shares.BaseFragment;
+import ruifu.com.shares.R;
+
+public class PositionFragment extends BaseFragment implements View.OnClickListener{
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_position, container, false);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
